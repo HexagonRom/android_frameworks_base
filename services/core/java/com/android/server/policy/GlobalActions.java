e9d2f5e85b9b5672241332bc9edbc507fd1cad1b
qjohn <townex22@gmail.com>
2017-07-31 22:13:47 +0700

    GlobalActions: Dismiss dialog on the screenshot long-press action
    * The missing part of http://gerrit.aicp-rom.com/#/c/32173/
    * Fix code style
    
    Change-Id: I403a4d88b5cb4842142d7e9a18ac3f15ef547737

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -623,6 +623,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         @Override
         public boolean onLongPress() {
+            mHandler.sendEmptyMessage(MESSAGE_DISMISS);
             takeScreenshot(mScreenshotSelectedRegion);
             return false;
         }
@@ -962,21 +963,21 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         public void setScreenshotType(int screenshotType) {
             if (Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SCREENSHOT_TYPE, 0) == 1) {
-            mScreenshotSelectedRegion = screenshotType;
+                    Settings.System.SCREENSHOT_TYPE, 0) == 1) {
+                mScreenshotSelectedRegion = screenshotType;
             } else {
-            mScreenshotFullscreen = screenshotType;
+                mScreenshotFullscreen = screenshotType;
             }
         }
 
         @Override
         public void run() {
-        if (Settings.System.getInt(mContext.getContentResolver(),
-              Settings.System.SCREENSHOT_TYPE, 0) == 1) {
-           takeScreenshot(mScreenshotSelectedRegion);
-        } else {
-           takeScreenshot(mScreenshotFullscreen);
-           }
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.SCREENSHOT_TYPE, 0) == 1) {
+                takeScreenshot(mScreenshotSelectedRegion);
+            } else {
+                takeScreenshot(mScreenshotFullscreen);
+            }
         }
     }
 

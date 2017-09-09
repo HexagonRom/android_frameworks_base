662f9f80ab11b8918ae1c8d6b5c352b130808420
xyyx <xyyx@mail.ru>
2017-09-09 15:12:48 +0800

    base: Lockscreen Charging info  (3/3)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4056,6 +4056,12 @@ public final class Settings {
 
         /**
          * --- HexagonRom System settings end
+        * Whether to show the battery info on the lockscreen while charging
+        * @hide
+        */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
+        /**
          *  Add new system settings above this comment
          */
 

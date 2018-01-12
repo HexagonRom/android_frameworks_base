0ded238cd37651c379fcd1ae25a98577292170f2
ezio84 <brabus84@gmail.com>
2018-01-12 10:54:32 +0100

    Multi ScreenRecord tile, blinking stop button, screenrecord service improvements [1/2]

--- core/java/android/view/WindowManagerPolicy.java
+++ core/java/android/view/WindowManagerPolicy.java
@@ -1752,4 +1752,9 @@ public interface WindowManagerPolicy {
      * @return true if ready; false otherwise.
      */
     boolean canDismissBootAnimation();
+
+    /**
+     * Call screen record from WindowManager.
+     */
+    public void screenRecordAction(int mode);
 }

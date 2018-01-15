AICP/frameworks_base

[1/2] base: SystemUI: simple switch between light and dark style
TODO: decide what to do with volume dialog
let it follow global style -> dark or fix
on default light theme

Change-Id: Ia5497c66b839431526fc7fde8777ec39d2d99730

packages/SystemUI/src/com/android/systemui/SystemUIApplication.java
@@ -27,6 +27,7 @@ import android.content.res.Configuration;
 import android.os.Process;
 import android.os.SystemProperties;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.ArraySet;
 import android.util.Log;
 
@@ -113,7 +114,10 @@ public class SystemUIApplication extends Application implements SysUiServiceProv
         // Set the application theme that is inherited by all services. Note that setting the
         // application theme in the manifest does only work for activities. Keep this in sync with
         // the theme set there.
-        setTheme(R.style.systemui_theme);
+        boolean darkStyle = Settings.System.getIntForUser(
+                getContentResolver(), Settings.System.QS_STYLE_DARK, 0,
+                UserHandle.USER_CURRENT) == 1;
+        setTheme(darkStyle ? R.style.systemui_theme_dark : R.style.systemui_theme);
 
         SystemUIFactory.createFromConfig(this);
 

HexagonRom/android_frameworks_base

packages/SystemUI/src/com/android/systemui/SystemUIApplication.java
@@ -28,6 +28,7 @@ import android.os.Process;
 import android.os.SystemProperties;
 import android.os.Trace;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.ArraySet;
 import android.util.TimingsTraceLog;
 import android.util.Log;
@@ -114,7 +115,10 @@ public class SystemUIApplication extends Application implements SysUiServiceProv
         // Set the application theme that is inherited by all services. Note that setting the
         // application theme in the manifest does only work for activities. Keep this in sync with
         // the theme set there.
-        setTheme(R.style.Theme_SystemUI);
+        boolean darkStyle = Settings.System.getIntForUser(
+                getContentResolver(), Settings.System.QS_STYLE_DARK, 0,
+                UserHandle.USER_CURRENT) == 1;
+        setTheme(darkStyle ? R.style.systemui_theme_dark : R.style.Theme_SystemUI);
 
         SystemUIFactory.createFromConfig(this);
 
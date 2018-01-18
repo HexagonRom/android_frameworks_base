AICP/frameworks_base

SystemUI: CollapsedStatusBarFragment: create setting observer in onCreate
Change-Id: If9f47137aecff3c7a9b6101a72ef3555887cbfce

packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -98,7 +98,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             updateSettings(true);
         }
     }
-    private AicpSettingsObserver mAicpSettingsObserver = new AicpSettingsObserver(mHandler);
+    private AicpSettingsObserver mAicpSettingsObserver;
 
     private SignalCallback mSignalCallback = new SignalCallback() {
         @Override
@@ -113,7 +113,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mKeyguardMonitor = Dependency.get(KeyguardMonitor.class);
         mNetworkController = Dependency.get(NetworkController.class);
         mStatusBarComponent = SysUiServiceProvider.getComponent(getContext(), StatusBar.class);
-        mAicpSettingsObserver.observe();
+        mAicpSettingsObserver = new AicpSettingsObserver(mHandler);
+        mOmniSettingsObserver.observe();
     }
 
     @Override

HexagonRom/android_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -96,7 +96,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             updateSettings(true);
         }
     }
-    private HexSettingsObserver mHexSettingsObserver = new HexSettingsObserver(mHandler);
+    private HexSettingsObserver mHexSettingsObserver;
 
     private SignalCallback mSignalCallback = new SignalCallback() {
         @Override
@@ -111,7 +111,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mKeyguardMonitor = Dependency.get(KeyguardMonitor.class);
         mNetworkController = Dependency.get(NetworkController.class);
         mStatusBarComponent = SysUiServiceProvider.getComponent(getContext(), StatusBar.class);
-        mHexSettingsObserver.observe();
+        mHexSettingsObserver = new HexSettingsObserver(mHandler);
     }
 
     @Override
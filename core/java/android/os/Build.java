omnirom/android_frameworks_base_old

[2/2] base: allow device to override *caugh*caugh* prop
Change-Id: Ib51a5e5ae87b004053dcf1c01d8aa6cf3ef63b65

core/java/android/os/Build.java
@@ -213,6 +213,12 @@ public class Build {
                 "ro.build.version.security_patch", "");
 
         /**
+         * The user-visible omni security patch level.
+         */
+        public static final String SECURITY_PATCH_OMNI = SystemProperties.get(
+                "ro.omni.version.security_patch", "");
+
+        /**
          * The user-visible SDK version of the framework in its raw String
          * representation; use {@link #SDK_INT} instead.
          *

packages/SettingsLib/src/com/android/settingslib/DeviceInfoUtils.java
@@ -153,9 +153,13 @@ public class DeviceInfoUtils {
     }
 
     public static String getSecurityPatch() {
+        String patchOmni = Build.VERSION.SECURITY_PATCH_OMNI;
         String patch = Build.VERSION.SECURITY_PATCH;
         if (!"".equals(patch)) {
             try {
+                if (!"".equals(patchOmni)){
+                    patch = patchOmni;
+                }
                 SimpleDateFormat template = new SimpleDateFormat("yyyy-MM-dd");
                 Date patchDate = template.parse(patch);
                 String format = DateFormat.getBestDateTimePattern(Locale.getDefault(), "dMMMMyyyy");

HexagonRom/android_frameworks_base

core/java/android/os/Build.java
@@ -232,6 +232,13 @@ public class Build {
                 "ro.build.version.security_patch", "");
 
         /**
+         * The user-visible real security patch level.
+         * @hide
+         */
+        public static final String SECURITY_PATCH_REAL = SystemProperties.get(
+                "ro.build.version.real_security_patch", "");
+
+        /**
          * The user-visible SDK version of the framework in its raw String
          * representation; use {@link #SDK_INT} instead.
          *

packages/SettingsLib/src/com/android/settingslib/DeviceInfoUtils.java
@@ -153,9 +153,13 @@ public class DeviceInfoUtils {
     }
 
     public static String getSecurityPatch() {
+        String patchReal = Build.VERSION.SECURITY_PATCH_REAL;
         String patch = Build.VERSION.SECURITY_PATCH;
         if (!"".equals(patch)) {
             try {
+                if (!"".equals(patchReal)){
+                    patch = patchReal;
+                }
                 SimpleDateFormat template = new SimpleDateFormat("yyyy-MM-dd");
                 Date patchDate = template.parse(patch);
                 String format = DateFormat.getBestDateTimePattern(Locale.getDefault(), "dMMMMyyyy");